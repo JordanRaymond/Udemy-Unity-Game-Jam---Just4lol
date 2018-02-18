766df3d019ad8dab6b2f24041f15a7b42ce1a08b
anuthams <anutham.suresh@gmail.com>
2018-02-18 23:14:31 +1300

    added latest project files with vertical thrust

---
+++ do-you-know-da-wae/Assets/_Scripts/Controller/ControllerStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Controller/Stats")]
+public class ControllerStats : ScriptableObject
+{
+    public float moveSpeed = 4;
+    public float boostSpeed = 6;
+    public float drag = 4;
+    public float jumpTrust = 4;
+    public float downForce = 10;
+
+    public float torque = 8;
+    public float maxAngularVelocity = 7;
+    public float angularDrag = 4;
+
+    public float rotationAccelerationSpeed = 0.1f;
+    public float rotationUpForce = 5f;
+}
